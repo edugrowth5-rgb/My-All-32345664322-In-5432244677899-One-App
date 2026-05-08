8f01e6621898aabfba87fc377651a2af299c0123
edugrowth5-rgb <edugrowth5@gmail.com>
2026-05-08 16:34:55 +0530

    Update QPsw.js

--- QPsw.js
+++ QPsw.js
@@ -8,6 +8,7 @@ const urlsToCache = [
   './QPchapter1.js',
   './QPchapter2.js',
   './QPgkquiz.js',
+  './AllInOne.jpg',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
 ];
 
